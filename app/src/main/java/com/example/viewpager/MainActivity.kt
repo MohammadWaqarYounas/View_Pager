7e77c7a99f21b7c1bfbbca2eaa974616f707608a
MohammadWaqarYounas <waqary268@gmail.com>
2022-12-14 15:35:36 +0500

    data binding implemented

--- app/src/main/java/com/example/viewpager/MainActivity.kt
+++ app/src/main/java/com/example/viewpager/MainActivity.kt
@@ -4,27 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.ViewPager
 import androidx.viewpager2.widget.ViewPager2
+import com.example.viewpager.databinding.ActivityMainBinding
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
-    lateinit var viewPager: ViewPager
-    lateinit var tabs: TabLayout
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         val adaptor =ViewPagerAdapter(supportFragmentManager)
         adaptor.addFragment(FragmentOne(),"One")
         adaptor.addFragment(FragmentTwo(),"Two")
         adaptor.addFragment(FragmentThree(),"Three")
-        viewPager=findViewById(R.id.viewpager)
-        tabs=findViewById(R.id.tablayout)
-        viewPager.adapter=adaptor
-        tabs.setupWithViewPager(viewPager)
-
+        binding.viewpager.adapter=adaptor
+        binding.tablayout.setupWithViewPager(binding.viewpager)
 
     }
 }
